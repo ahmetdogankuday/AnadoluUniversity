d8b49e07bad9c38a9a27744e9d8a8ec0bc2fa574
ahmetdogankuday <161274965+ahmetdogankuday@users.noreply.github.com>
2024-03-24 05:49:49 +0300

    Add files via upload

---
+++ Veri Madenciliği Ödev1.R
@@ -0,0 +1,54 @@
+# Print
+print("Ahmet Doğan Kuday")
+print("Anadolu Üniversitesi")
+
+
+# Matematik 
+5 + 5
+5 * 5
+5 ^ 5
+5 + (2 * 2)^2
+(5 + 5) / 2 + 5
+
+# Değişken Tanımlama ve Sınıf
+x = 2 
+x <- 2
+y = "Merhaba Dünya"
+
+x
+x * 4
+x + 2
+
+print(x)
+print(y)
+
+class(x)
+class(y)
+
+# Combine Alıştırma
+x <- c(1, 2, 3, 4)
+x
+x + 5
+x * 5
+x + c(4, 3, 2, 1)
+length(x)
+
+y = x + c(1, 2, 3, 4)
+y
+length(y)
+
+
+# Length
+name = "Ahmet Doğan"
+name2 = c("Ahmet","Doğan")
+name
+name2
+length(name)
+length(name2)
+
+# Hata Örneği
+name2 + 10
+
+## Yapı
+str(x)
+str(y)
